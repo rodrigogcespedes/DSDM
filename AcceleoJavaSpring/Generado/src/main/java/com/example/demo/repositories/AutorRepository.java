3ae60fdd2b683723bec8aae1df82979fd5ddd492
Rodrigo <rodrigogcespedes@gmail.com>
2020-11-10 19:15:14 -0300

    Arreglado Swagger

--- AcceleoJavaSpring/Generado/src/main/java/com/example/demo/repositories/AutorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-import com.example.demo.entities.Paquete1.Autor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AutorRepository extends BaseRepository<Autor, Long>{
-
-}
--- AcceleoJavaSpring/Generado/src/main/java/com/example/demo/services/PersonaService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.services;
-import com.example.demo.entities.Paquete1.Persona;
-
-public interface PersonaService extends BaseService<Persona,Long>{
-}
-
